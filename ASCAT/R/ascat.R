cff66f45222e3e22bc3c442284e9d100bc068c68
karolno <karolno@gmail.com>
2022-01-25 13:59:38 -0500

    fix of ascat.plotSunrise

--- ASCAT/R/ascat.R
+++ ASCAT/R/ascat.R
@@ -1556,8 +1556,10 @@ ascat.plotSunrise<-function(d, psi_opt1, rho_opt1, minim=T){
   purity_min<-as.numeric(colnames(d)[1])
   purity_max<-as.numeric(colnames(d)[ncol(d)])
   
-  axis(1, at = seq(0, 1, by = 1/(ploidy_max-1)), labels = seq(0, ploidy_max, by = 1))
-  axis(2, at = seq(0, 1/purity_max, by = 1/3/purity_max), labels = seq(0, purity_max, by = 3/10))
+  axis(1, at = seq(0, 1, by = 1/(ploidy_max-1)), labels = seq(ploidy_min, ploidy_max, by = 1))
+  # axis(2, at = seq(0, 1/purity_max, by = 1/3/purity_max))#, labels = seq(purity_min, purity_max, by = 3/10))
+  axis(2, at = seq(0, 1/purity_max, length.out = 5), labels = seq(purity_min, purity_max, length.out = 5))
+  
   
   if(psi_opt1>0 && rho_opt1>0){
     points((psi_opt1-ploidy_min)/(ploidy_max-1),(rho_opt1-purity_min)/(1/purity_max),col="green",pch=4, cex = 2)
